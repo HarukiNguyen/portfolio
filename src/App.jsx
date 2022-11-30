ed7c9df06f0f75cc287f1af68f2a8df18471330a
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-11-30 20:33:06 +0700

    feat: :sparkles: setup content layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import Header from "./layouts/Header";
+import Content from "./layouts/Content";
+import {Routes, Route} from "react-router-dom"
 
 function App() {
   return (
     <div className="container px-3 text-slate-900 font-body leading-normal ">
       <Header />
+      <Routes>
+        <Route path="/" element={<Content />} />
+      </Routes>
     </div>
   );
 }
