ccc910ad4a50393e854c063c665852afef6f9936
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-11-30 20:27:38 +0700

    feat: :sparkles: add href for social media links

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -7,14 +7,17 @@ const socialMedias = [
   {
     name: "github",
     iconUrl: githubIcon,
+    url: "https://github.com/HarukiNguyen",
   },
   {
     name: "linkedin",
     iconUrl: linkedinIcon,
+    url: "https://www.linkedin.com/in/haruki-nguyen-799b0a255/",
   },
   {
     name: "email",
     iconUrl: emailIcon,
+    url: "mailto:nmd03pvt@gmail.com",
   },
 ];
 
