37cdbe629ec0bb17cc605d698637927953eb96ac
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-11-30 20:28:14 +0700

    feat: :lipstick: style for header

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -21,27 +21,45 @@ const socialMedias = [
   },
 ];
 
-const navs = ["About", "Projects", "Contact"];
+const navs = ["about", "projects", "contact"];
 
 function Header() {
   const socialMediaItems = socialMedias.map((socialMedia) => {
-    const { name, iconUrl } = socialMedia;
+    const { name, iconUrl, url } = socialMedia;
     return (
-      <li key={name}>
-        <img src={iconUrl} alt={name} />
+      <li key={name} className="px-3">
+        <a href={url} title={name} className="block p-2 rounded-full focus:hocus-state hover:hocus-state cursor-pointer">
+          <img src={iconUrl} alt={name} />
+        </a>
       </li>
     );
   });
 
   const navItems = navs.map((nav) => {
-    return <li key={nav}>{nav}</li>;
+    return (
+      <li key={nav} className="py-1 text-slate-600 hover:font-bold hover:text-slate-800">
+        <a href={`/${nav}`} className="rounded px-2 capitalize focus:hocus-state">
+          {nav}
+        </a>
+      </li>
+    );
   });
 
   return (
-    <header className="">
-      <img src={logo} alt="logo" className="" />
-      <ul className="col-span-2">{socialMediaItems}</ul>
-      <ul>{navItems}</ul>
+    <header className="pt-3">
+      <div className="flex justify-between">
+        <div className="flex items-center">
+          <a
+            href="/"
+            alt="logo"
+            className="px-2 py-1 rounded focus:bg-slate-100 focus:outline-none cursor-pointer"
+          >            
+            <img src={logo} alt="logo" />
+          </a>
+        </div>
+        <ul className="flex items-center">{socialMediaItems}</ul>
+      </div>
+      <ul className="inline-block pt-7">{navItems}</ul>
     </header>
   );
 }
