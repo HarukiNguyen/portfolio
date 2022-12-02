3340671b7bfbe0f6b8bed86910fdf7f4d8e7167b
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-12-02 10:22:19 +0700

    feat: :lipstick: change nav item layout

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -6,7 +6,7 @@ const navs = ["about", "knowledge", "projects", "contact"];
 function Header() {
   const navItems = navs.map((nav) => {
     return (
-      <li key={nav} className="py-2">
+      <li key={nav} className="py-2 w-2/4">
         <a
           href={`/#${nav}`}
           className="rounded px-2 text-xl capitalize text-slate-600  transition-color focus:hocus-state hover:hocus-state active:bg-slate-200"
@@ -25,7 +25,7 @@ function Header() {
           <SocialMedias />
         </div>
       </div>
-      <ul className="flex justify-between pt-3">{navItems}</ul>
+      <ul className="flex flex-wrap justify-between pt-3">{navItems}</ul>
     </header>
   );
 }
