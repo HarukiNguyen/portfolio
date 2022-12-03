cc5d111145fd8548be1c8828d7d043e324f8c30d
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-12-03 15:30:27 +0700

    refactor: :recycle: optimize About component

---
+++ src/components/About/About.jsx
@@ -0,0 +1,33 @@
+import { aboutData } from "./aboutData";
+import Avatar from "./Avatar";
+
+function About() {
+  const aboutTexts = aboutData.map((about, id) => {
+    return (
+      <p
+        key={id}
+        className="indent-5 pb-5 paragraph-dot before:left-0 before:top-[0.5em]"
+      >
+        {about}
+      </p>
+    );
+  });
+
+  return (
+    <section
+      className="pb-11 animate-fadeIn [animation-delay:0.75s] opacity-0"
+      id="about"
+    >
+      <h1 className="heading leading-tight text-4xl">
+        Hi, I'm Haruki Nguyen, I am a{" "}
+        <span className="inline-block capitalize text-underline">
+          web developer
+        </span>
+      </h1>
+      <div className="py-10">{aboutTexts}</div>
+      <Avatar />
+    </section>
+  );
+}
+
+export default About;
---
+++ src/components/About/Avatar.jsx
@@ -0,0 +1,13 @@
+import avt from "../../assets/images/cat.jpg";
+
+function Avatar() {
+  return (
+    <div className="flex justify-center">
+      <div className="relative w-9/12 before:absolute before:w-full before:h-full before:bg-slate-300 before:-translate-x-1 before:-translate-y-1">
+        <img src={avt} alt="avatar" className="translate-x-3 translate-y-3" />
+      </div>
+    </div>
+  );
+}
+
+export default Avatar;
