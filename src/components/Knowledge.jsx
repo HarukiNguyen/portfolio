c77e641b0e2b5eb87a2be3c7fc26a5d34167d541
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-12-03 11:12:50 +0700

    feat: :sparkles: add css ultilities

--- src/components/Knowledge.jsx
+++ src/components/Knowledge.jsx
@@ -12,7 +12,7 @@ function Knowledge() {
   const knowledgeItems = knowledgeData.map((skill) => (
     <li key={skill} className="w-2/4 pb-6 pl-7">
       <div className="">
-        <span className="relative before:absolute before:w-[6px] before:h-[6px] before:rounded-full before:-left-4 before:top-2/4 before:-translate-y-2/4 before:bg-slate-400 inline-block px-3 py-2 rounded bg-slate-50 shadow">
+        <span className="paragraph-dot inline-block px-3 py-2 rounded bg-slate-50 shadow before:top-2/4 before:-translate-y-2/4  before:-left-4">
           {skill}
         </span>
       </div>
