6d8eca8573bc089954d6fef2c12f861a4388b58e
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-12-03 15:08:52 +0700

    refactor: :art: optimize Header.jsx code

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -3,13 +3,9 @@ import SocialMedias from "../components/SocialMedias";
 
 function Header() {
   return (
-    <header>
-      <div className="-mx-3 px-3 py-3 bg-slate-200 shadow-md animate-fadeIn opacity-0">
-        <div className="flex justify-between">
-          <Logo />
-          <SocialMedias />
-        </div>
-      </div>
+    <header className="flex justify-between -mx-3 px-3 py-3 bg-slate-200 shadow-md animate-fadeIn opacity-0">
+      <Logo />
+      <SocialMedias />
     </header>
   );
 }
