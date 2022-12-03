0fccfd42d45ce1b5ea2fea9fc707928768d93592
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-12-03 14:36:54 +0700

    feat: :sparkles: add function to apply animation

---
+++ src/ultilities/visibleAni.js
@@ -0,0 +1,27 @@
+export default function visibleAni(els, animation) {
+  function addAni(entry) {
+    if (entry.isIntersecting) {
+      entry.target.classList.add(animation);
+    }
+  }
+
+  if (Array.isArray(els)) {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        addAni(entry)
+      });
+    });
+
+    els.forEach((projectEl) => {
+      observer.observe(projectEl);
+    });
+  } else {
+    const observer = new IntersectionObserver((entries) => {
+      const entry = entries[0];
+
+      addAni(entry);
+    })
+
+    observer.observe(els)
+  }
+}
