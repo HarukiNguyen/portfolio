bd4638501a5697ecc1733c84a2e01a1400c82919
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-12-03 15:18:50 +0700

    refactor: optimzie SocialMedias component

---
+++ src/components/SocialMedias/socialMediasData.js
@@ -0,0 +1,23 @@
+import emailIcon from "../../assets/icons/email.svg";
+import githubIcon from "../../assets/icons/github.svg";
+import linkedinIcon from "../../assets/icons/linkedin.svg";
+
+const socialMediasData = [
+  {
+    name: "github",
+    iconUrl: githubIcon,
+    url: "https://github.com/HarukiNguyen",
+  },
+  {
+    name: "linkedin",
+    iconUrl: linkedinIcon,
+    url: "https://www.linkedin.com/in/haruki-nguyen-799b0a255/",
+  },
+  {
+    name: "email",
+    iconUrl: emailIcon,
+    url: "mailto:nmd03pvt@gmail.com",
+  },
+];
+
+export { socialMediasData };
