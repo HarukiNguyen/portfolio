e8716b1b71373334704dbbd172bdfed174d0de13
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-12-03 11:13:34 +0700

    feat: :sparkles: update about content and decorate it

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,15 +1,18 @@
 import avt from "../assets/images/cat.jpg";
 
 const aboutDatas = [
-  "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil a distinctio ipsam voluptatum fugit facilis ea deserunt explicabo impedit numquam.",
-  "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil a distinctio ipsam voluptatum fugit facilis ea deserunt explicabo impedit numquam.",
-  "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil a distinctio ipsam voluptatum fugit facilis ea deserunt explicabo impedit numquam.",
+  "My web development journey started when I was 17 years old.",
+  "These days, I decided not to go to the university because I feel more comfortable learning it myself and discovering every piece of web technology.",
+  "Even though I'm self-taught, I still believe that I have enough ability to give you what you need.",
 ];
 
 function About() {
   const aboutTexts = aboutDatas.map((about, id) => {
     return (
-      <p key={id} className="indent-3 pb-5">
+      <p
+        key={id}
+        className="indent-5 pb-5 paragraph-dot before:left-0 before:top-[0.5em]"
+      >
         {about}
       </p>
     );
@@ -17,13 +20,13 @@ function About() {
 
   return (
     <section className="pb-11" id="about">
-      <h1 className="pb-11 heading leading-tight text-4xl">
+      <h1 className="heading leading-tight text-4xl">
         Hi, I'm Haruki Nguyen, I am a{" "}
         <span className="inline-block capitalize text-underline">
           web developer
         </span>
       </h1>
-      {aboutTexts}
+      <div className=" py-10">{aboutTexts}</div>
       <div className="flex justify-center">
         <div className="relative w-9/12 before:absolute before:w-full before:h-full before:bg-slate-300 before:-translate-x-1 before:-translate-y-1">
           <img src={avt} alt="avatar" className="translate-x-3 translate-y-3" />
