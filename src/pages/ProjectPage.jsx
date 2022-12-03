bb83250f3f41b1708f2f8145254e5f10cebf261e
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-12-03 20:26:48 +0700

    feat: :sparkles: basic logic for render project page based on params

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -1,7 +1,12 @@
+import { projects } from "../components/Projects/projectsData";
 import { useParams } from "react-router-dom";
 
 function ProjectPage() {
   const params = useParams();
+
+  const project = projects.find(
+    (project) => `/${params.project}` === project.url
+  );
 
   return <h3>Have a nice day</h3>;
 }
