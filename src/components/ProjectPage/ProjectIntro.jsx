d19f0452cf1378ba022e86e69c3f92e28010f7b5
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-12-07 14:44:08 +0700

    feat: :lipstick: style for ProjectIntro component

--- src/components/ProjectPage/ProjectIntro.jsx
+++ src/components/ProjectPage/ProjectIntro.jsx
@@ -1,26 +1,56 @@
-function ProjectIntro({ detailName, url, purpose, type, tech, introImg }) {
+import linkIcon from "../../assets/icons/link.svg";
+
+function ProjectIntro({ detailName, url, purpose, type, stack, introImg }) {
   return (
     <section>
-      <h1>
-        <a href={url} alt={detailName}>
-          {detailName}
-        </a>
-      </h1>
-      <p>{purpose}</p>
-      <dl>
-        <dt>Type</dt>
-        <dd>{type}</dd>
+      <a
+        href={url}
+        alt={detailName}
+        className="group px-2 py-1 -mx-2 -my-1 hover:text-slate-600 focus:bg-slate-50 focus:outline-none transition-color"
+      >
+        <h1 className="inline">{detailName}</h1>
+        <div className="inline-flex items-center">
+          <img
+            src={linkIcon}
+            alt="Link icon"
+            className="block pl-3 w-10 group-hover:scale-110 group-focus:scale-110 transition-transform"
+          />
+        </div>
+      </a>
+      <p className="py-7">{purpose}</p>
+      <dl className="flex">
+        {[type, stack].map((desc, index) => {
+          let title;
+          let content;
+          if (index === 0) {
+            title = "Type";
+            content = desc;
+          } else {
+            title = "Stack";
+            content = desc.map((techItem) => (
+              <li key={techItem} className="py-1">
+                {techItem}
+              </li>
+            ));
+          }
 
-        <dt>Tech</dt>
-        <dd>
-          <ul>
-            {tech.map((techItem) => (
-              <li key={techItem}>{techItem}</li>
-            ))}
-          </ul>
-        </dd>
+          return (
+            <div key={title} className="pr-14">
+              <dt className="pb-2 font-bold">{title}</dt>
+              <dd>
+                <ul className="list-disc">{content}</ul>
+              </dd>
+            </div>
+          );
+        })}
       </dl>
-      <img src={introImg} alt="Introduce image" />
+      <div className="py-7">
+        <img
+          src={introImg}
+          alt="Introduce image"
+          className="rounded-md shadow-lg"
+        />
+      </div>
     </section>
   );
 }
