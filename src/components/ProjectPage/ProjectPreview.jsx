2dbfef21f88b14fd471dfedcb9a2ab423ebc33b0
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-12-07 15:48:50 +0700

    feat: :lipstick: style for ProjectPreview

--- src/components/ProjectPage/ProjectPreview.jsx
+++ src/components/ProjectPage/ProjectPreview.jsx
@@ -2,10 +2,14 @@ function ProjectPreview({ previewImg }) {
   return (
     <section>
       <h2>Preview images</h2>
-      <ul>
+      <ul className="pt-10 flex justify-center flex-wrap">
         {previewImg.map((image, index) => (
-          <li key={index}>
-            <img src={image} alt="Preview image" />
+          <li key={index} className="w-10/12 pb-10">
+            <img
+              src={image}
+              alt="Preview image"
+              className="rounded-md shadow-3xl"
+            />
           </li>
         ))}
       </ul>
