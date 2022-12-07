134ea4f9acf74e9c0900bac04f8aadfded5b756e
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-12-07 20:21:38 +0700

    feat(responsive): :iphone: tablet and desktop: Header -> more padding top and bottom

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -3,7 +3,7 @@ import SocialMedias from "../components/SocialMedias";
 
 function Header() {
   return (
-    <header className="flex justify-between -mx-3 px-3 py-3 bg-slate-200 shadow-md animate-fadeIn opacity-0">
+    <header className="flex justify-between -mx-3 px-3 py-3 tablet:py-5 animate-fadeIn opacity-0">
       <Logo />
       <SocialMedias />
     </header>
