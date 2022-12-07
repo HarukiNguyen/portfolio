f9bfb4863f32141a4f541c2d489a31443bae539e
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-12-07 20:22:32 +0700

    feat(responsive): :iphone: tablet and desktop: <main> -> more padding top and bottom

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="container px-3 tablet:px-6 text-slate-900 font-body leading-relaxed">
       <Header />
-      <main className="py-20">
+      <main className="py-10 tablet:py-16">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/:project" element={<ProjectPage />} />
