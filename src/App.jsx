2c7b83cad56d9a0643b6d209435d1475b3d52d28
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-12-07 20:32:39 +0700

    refactor(HTML): :recycle: App: move header and footer out of the .container

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,18 @@ import ProjectPage from "./pages/ProjectPage";
 
 function App() {
   return (
-    <div className="container px-3 tablet:px-6 text-slate-900 font-body leading-relaxed">
+    <>
       <Header />
-      <main className="py-10 tablet:py-16">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/:project" element={<ProjectPage />} />
-        </Routes>
-      </main>
+      <div className="container px-3 tablet:px-6 text-slate-900 font-body leading-relaxed">
+        <main className="py-10 tablet:py-16">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/:project" element={<ProjectPage />} />
+          </Routes>
+        </main>
+      </div>
       <Footer />
-    </div>
+    </>
   );
 }
 
