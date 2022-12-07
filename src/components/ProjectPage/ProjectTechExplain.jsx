b7a103493ad9a869106fb91d2bd754590e72a662
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-12-07 15:25:23 +0700

    feat: :lipstick: style for ProjectTechExplain component

--- src/components/ProjectPage/ProjectTechExplain.jsx
+++ src/components/ProjectPage/ProjectTechExplain.jsx
@@ -1,10 +1,16 @@
 function ProjectTechExplain({ techAndExplain }) {
+  const explanativeLength = techAndExplain.length;
+  const itemClassNames =
+    explanativeLength > 1 ? "pb-5 paragraph-dot pd-lt" : "";
+
   return (
     <section>
       <h2>Web stack and explanation</h2>
       <ul>
         {techAndExplain.map((explainPara, index) => (
-          <li key={index}>{explainPara}</li>
+          <li key={index} className={`${itemClassNames}`}>
+            <p>{explainPara}</p>
+          </li>
         ))}
       </ul>
     </section>
