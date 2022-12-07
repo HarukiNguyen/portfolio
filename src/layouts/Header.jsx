b554e4e0d806bd8994ab1644aa9a5959aa4152f1
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-12-07 20:36:05 +0700

    feat(styles): :lipstick: Header: add background, box shadow, and remove negative margins

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -3,7 +3,7 @@ import SocialMedias from "../components/SocialMedias";
 
 function Header() {
   return (
-    <header className="flex justify-between -mx-3 px-3 py-3 tablet:py-5 animate-fadeIn opacity-0">
+    <header className="flex justify-between bg-slate-100 shadow-md px-3 py-3 tablet:py-5 animate-fadeIn opacity-0">
       <Logo />
       <SocialMedias />
     </header>
