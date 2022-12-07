61ea51568429669f437e274b69a148311008ba4c
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-12-07 16:15:43 +0700

    feat: :dizzy: Projects: add animation for heading

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,10 +1,18 @@
+import { useEffect, useRef } from "react";
 import { projectsData } from "../../data/projectsData";
+import visibleAni from "../../ultilities/visibleAni";
 import ProjectList from "./ProjectList";
 
 function Projects() {
+  const headingRef = useRef(null);
+
+  useEffect(() => {
+    visibleAni(headingRef.current, "animate-fadeIn");
+  }, []);
+
   return (
     <section id="projects">
-      <h2>
+      <h2 ref={headingRef}>
         What I've been working on <span>💻</span>
       </h2>
       <ProjectList projectsData={projectsData} />
