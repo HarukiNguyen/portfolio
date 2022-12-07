2aa74735423fb4e671d26115add53ac53a26634b
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-12-07 16:46:44 +0700

    fix: :dizzy: fix transition class spelling mistake

--- src/components/ProjectPage/ProjectIntro.jsx
+++ src/components/ProjectPage/ProjectIntro.jsx
@@ -7,7 +7,7 @@ function ProjectIntro({ detailName, url, purpose, type, stack, introImg }) {
         <a
           href={url}
           alt={detailName}
-          className="group px-2 py-1 -mx-2 -my-1 hover:text-slate-600 focus:bg-slate-50 focus:outline-none transition-color"
+          className="group px-2 py-1 -mx-2 -my-1 hover:text-slate-600 focus:bg-slate-50 focus:outline-none transition-colors"
         >
           <h1 className="inline pb-0">{detailName}</h1>
           <div className="inline-flex items-center">
