4578bb555668eb90547ee76b53b5519b8a1672ff
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-12-08 09:49:25 +0700

    feat(responsive): :iphone: Header: add .container

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -3,9 +3,11 @@ import SocialMedias from "../components/SocialMedias";
 
 function Header() {
   return (
-    <header className="flex justify-between bg-slate-100 shadow-md px-3 py-3 tablet:py-5 tablet:px-5 animate-fadeIn opacity-0">
-      <Logo />
-      <SocialMedias />
+    <header className="bg-slate-100 shadow-md animate-fadeIn opacity-0">
+      <div className="container flex justify-between px-3 py-3 tablet:py-5 tablet:px-5 ">
+        <Logo />
+        <SocialMedias />
+      </div>
     </header>
   );
 }
