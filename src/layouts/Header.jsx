c4abbd8f7a6254b935fba6984c0932a615a7e6c8
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-12-08 13:46:13 +0700

    feat: :lipstick: Header: change background color

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -3,7 +3,7 @@ import SocialMedias from "../components/SocialMedias";
 
 function Header() {
   return (
-    <header className="bg-slate-100 shadow-md animate-fadeIn opacity-0">
+    <header className="bg-slate-200 shadow-md animate-fadeIn opacity-0">
       <div className="container flex justify-between px-3 py-3 tablet:py-5 tablet:px-5 ">
         <Logo />
         <SocialMedias />
