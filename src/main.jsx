2761321c4f360f9e611a91498c2f77d8ce1f65bb
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-12-09 14:46:34 +0700

    feat(UX): :children_crossing: add ScrollRestoration component

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./assets/styles/main.css";
+import ScrollRestoration from "./components/ScrollRestoration";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ScrollRestoration>
+        <App />
+      </ScrollRestoration>
     </BrowserRouter>
   </React.StrictMode>
 );
