1586c47f89a39263e2ce78d2d6539f89459e2c8f
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-12-10 14:37:29 +0700

    feat: ProjectInfo > adjust text hierachy

--- src/components/Projects/ProjectIcon.jsx
+++ src/components/Projects/ProjectIcon.jsx
@@ -1,7 +1,7 @@
 function ProjectIcon({ iconUrl, name }) {
   return (
     <span className="flex justify-center pb-3">
-      <img src={iconUrl} alt={`${name} icon`} className="w-8" />
+      <img src={iconUrl} alt={`${name} icon`} className="w-7" />
     </span>
   );
 }
--- src/components/Projects/ProjectInfo.jsx
+++ src/components/Projects/ProjectInfo.jsx
@@ -7,7 +7,7 @@ function ProjectInfo({ name, iconUrl, description, url, completed }) {
     <div className="absolute bottom-0 translate-y-2/4 bg-slate-50 left-2/4 -translate-x-2/4 w-10/12 px-7 py-6 rounded-3xl shadow-lg">
       <ProjectIcon iconUrl={iconUrl} name={name} />
       <ProjectName name={name} />
-      <p className="text-center pt-3">{description}</p>
+      <p className="text-center pt-3 text-slate-700 tablet:text-base ">{description}</p>
       <ProjectLink url={url} completed={completed} />
     </div>
   );
