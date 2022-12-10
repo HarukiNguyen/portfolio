25c9b1cae301f448bbfd18c41f56f3fd8f0a74e4
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-12-10 14:39:20 +0700

    refactor(responsive): App > keep font size at text-lg for desktop breakpoint

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import ProjectPage from "./pages/ProjectPage";
 
 function App() {
   return (
-    <div className=" tablet:text-lg desktop:text-xl text-slate-900 font-body leading-relaxed">
+    <div className=" tablet:text-lg text-slate-900 font-body leading-relaxed">
       <Header />
       <div className="container">
         <main className="py-10 tablet:py-12 laptop:py-20 desktop:py-28 px-3 tablet:px-6 laptop:px-12 desktop:px-28">
