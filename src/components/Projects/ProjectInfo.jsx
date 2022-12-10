de0516d96ac9aa974ee0076a234519742fe2c749
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-12-10 14:56:29 +0700

    feat: ProjectList, ProjectInfo > decrease space

--- src/components/Projects/ProjectInfo.jsx
+++ src/components/Projects/ProjectInfo.jsx
@@ -4,7 +4,7 @@ import ProjectName from "./ProjectName";
 
 function ProjectInfo({ name, iconUrl, description, url, completed }) {
   return (
-    <div className="absolute bottom-0 translate-y-2/4 bg-slate-50 left-2/4 -translate-x-2/4 w-10/12 px-12 py-7 rounded-3xl shadow-lg">
+    <div className="absolute bottom-0 translate-y-2/4 bg-slate-50 left-2/4 -translate-x-2/4 w-10/12 px-5 py-7 rounded-3xl shadow-lg">
       <ProjectIcon iconUrl={iconUrl} name={name} />
       <ProjectName name={name} />
       <p className="text-center py-6 text-slate-700 tablet:text-base ">{description}</p>
