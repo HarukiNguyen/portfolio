38d87c79a533554ea8286cdae804cb8d710e9e05
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-12-10 18:03:46 +0700

    feat(responsive): App > add max width for each breakpoint

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ import ProjectPage from "./pages/ProjectPage";
 
 function App() {
   return (
-    <div className=" tablet:text-lg text-slate-900 font-body leading-relaxed">
+    <div className="tablet:text-lg text-slate-900 font-body leading-relaxed">
       <Header />
-      <div className="container">
-        <main className="py-20 tablet:py-24 laptop:py-28 desktop:py-36 px-5 tablet:px-7 laptop:px-14 desktop:px-32">
+      <div className="min-[660px]:max-w-[630px] tablet:max-w-[720px] laptop:max-w-[960px] desktop:max-w-[1200px] mx-auto px-6">
+        <main className="py-20 tablet:py-24 laptop:py-28 desktop:py-36">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/:project" element={<ProjectPage />} />
