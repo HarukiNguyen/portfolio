9f2f6e83a29484b52fe4bd550293520eca3763d1
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-12-10 10:54:37 +0700

    refactor: :recycle: App --> move class names from .container to <main>

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ function App() {
   return (
     <>
       <Header />
-      <div className="container px-3 tablet:px-6 laptop:px-12 desktop:px-28 text-slate-900 font-body leading-relaxed">
-        <main className="py-10 tablet:py-12 laptop:py-20 desktop:py-28">
+      <div className="container">
+        <main className="py-10 tablet:py-12 laptop:py-20 desktop:py-28 px-3 tablet:px-6 laptop:px-12 desktop:px-28 text-slate-900 font-body leading-relaxed">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/:project" element={<ProjectPage />} />
