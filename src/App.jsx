848f3508d348f8cc612660469b52139be117c237
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-12-10 14:02:27 +0700

    refactor(responsive): :iphone: App --> increase font size on different viewport

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ import ProjectPage from "./pages/ProjectPage";
 
 function App() {
   return (
-    <>
+    <div className=" tablet:text-lg desktop:text-xl text-slate-900 font-body leading-relaxed">
       <Header />
       <div className="container">
-        <main className="py-10 tablet:py-12 laptop:py-20 desktop:py-28 px-3 tablet:px-6 laptop:px-12 desktop:px-28 text-slate-900 font-body leading-relaxed">
+        <main className="py-10 tablet:py-12 laptop:py-20 desktop:py-28 px-3 tablet:px-6 laptop:px-12 desktop:px-28">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/:project" element={<ProjectPage />} />
@@ -17,7 +17,7 @@ function App() {
         </main>
       </div>
       <Footer />
-    </>
+    </div>
   );
 }
 
