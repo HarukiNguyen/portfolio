4cc024d8772d4c98575bb7685df5b8e23dae0144
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-12-10 15:36:23 +0700

    feat: ProjectPrevew > use css columns property for preview images layout

--- src/components/ProjectPreview/PreviewImage.jsx
+++
@@ -1,9 +0,0 @@
-function PreviewImage({ image }) {
-  return (
-    <li className="w-10/12 tablet:w-full pb-10 tablet:pb-14 tablet:px-7">
-      <img src={image} alt="Preview image" className="mx-auto rounded-md shadow-3xl" />
-    </li>
-  );
-}
-
-export default PreviewImage;
--- src/components/ProjectPreview/ProjectPreview.jsx
+++ src/components/ProjectPreview/ProjectPreview.jsx
@@ -1,8 +1,5 @@
 import { useEffect, useRef } from "react";
 import visibleAni from "../../ultilities/visibleAni";
-import getCol from "./getCols";
-import getImgItems from "./getImgItems";
-import PreviewImageList from "./PreviewImageList";
 
 function ProjectPreview({ previewImg }) {
   const sectionRef = useRef(null);
@@ -11,12 +8,22 @@ function ProjectPreview({ previewImg }) {
     visibleAni(sectionRef.current, "animate-fadeIn");
   }, []);
 
-  const cols = getCol(getImgItems, previewImg, 2, 2);
+  const imgs = previewImg.map((img, index) => {
+    return (
+      <li key={index} className="pb-14 px-10 tablet:px-0">
+        <img
+          src={img}
+          alt="Preview image"
+          className="mx-auto rounded-md shadow-3xl"
+        />
+      </li>
+    );
+  });
 
   return (
     <section ref={sectionRef}>
       <h2 className="visually-hidden">Preview images</h2>
-      <PreviewImageList imageItems={cols} />
+      <ul className="tablet:columns-2 tablet:gap-14">{imgs}</ul>
     </section>
   );
 }
