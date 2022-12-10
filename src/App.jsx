c9c8c22fbf31e5b19eb79c39002edb0a1cb654d4
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-12-10 18:06:47 +0700

    feat: Tailwind css > add .grid-container ultility

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="tablet:text-lg text-slate-900 font-body leading-relaxed">
       <Header />
-      <div className="min-[660px]:max-w-[630px] tablet:max-w-[720px] laptop:max-w-[960px] desktop:max-w-[1200px] mx-auto px-6">
+      <div className="grid-container">
         <main className="py-20 tablet:py-24 laptop:py-28 desktop:py-36">
           <Routes>
             <Route path="/" element={<Home />} />
