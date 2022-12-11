3fd6970831f8024e5e56fad9d130d96055ea6186
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-12-11 22:26:58 +0700

    feat: Avatar > update style

--- src/components/About/Avatar.jsx
+++ src/components/About/Avatar.jsx
@@ -4,8 +4,8 @@ function Avatar() {
   return (
     <div className="flex justify-center">
       <div className="relative w-9/12">
-        <span className="absolute w-full h-full bg-indigo-500 -translate-x-3 -translate-y-3"></span>
-        <img src={avt} alt="avatar" className="translate-x-3 translate-y-3" />
+        <span className="absolute w-full h-full bg-indigo-500 -translate-x-4 -translate-y-4 desktop:-translate-x-5 desktop:-translate-y-5"></span>
+        <img src={avt} alt="avatar" className="translate-x-4 translate-y-4 desktop:translate-x-5 desktop:translate-y-5" />
       </div>
     </div>
   );
