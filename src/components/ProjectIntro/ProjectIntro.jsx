02c2c02eb46e9e9be45c9a8973dbb477dec4024c
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-12-11 13:56:49 +0700

    feat: ProjectIntro > use grid system

--- src/components/ProjectIntro/ProjectIntro.jsx
+++ src/components/ProjectIntro/ProjectIntro.jsx
@@ -3,19 +3,21 @@ import ProjectDetailName from "./ProjectDetailName";
 
 function ProjectIntro({ detailName, url, purpose, type, stack, introImg }) {
   return (
-    <section className="animate-fadeIn [animation-delay:0.75s] opacity-0">
-      <ProjectDetailName url={url} detailName={detailName} />
+    <section className="animate-fadeIn [animation-delay:0.75s] opacity-0 tablet:grid tablet:grid-cols-12">
+      <div className="tablet:col-span-9">
+        <ProjectDetailName url={url} detailName={detailName} />
 
-      {/* purpose, intro paragraph */}
-      <p className="para-max-w">{purpose}</p>
-      
-      <ProjectDesc descList={[type, stack]} />
+        {/* purpose, intro paragraph */}
+        <p className="para-max-w">{purpose}</p>
+
+        <ProjectDesc descList={[type, stack]} />
+      </div>
       {/* intro image */}
-      <div className="laptop:flex laptop:justify-center">
+      <div className="tablet:col-span-12 laptop:col-span-10">
         <img
           src={introImg}
           alt="Introduce image"
-          className="rounded-md shadow-lg laptop:w-10/12"
+          className="rounded-md shadow-lg"
         />
       </div>
     </section>
