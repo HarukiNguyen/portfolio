d94a4b707fbc2f5e53099a4d1447ac6dfc6c52a5
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-12-11 15:14:42 +0700

    feat: ProjectDetailName > change hover effect and link icon alignment

--- src/components/ProjectIntro/ProjectDetailName.jsx
+++ src/components/ProjectIntro/ProjectDetailName.jsx
@@ -6,10 +6,10 @@ function ProjectDetailName({ url, detailName }) {
       <a
         href={url}
         alt={detailName}
-        className="group rounded px-2 py-1 -mx-2 -my-1 hover:text-slate-600 focus:bg-slate-200 focus:outline-none transition-colors"
+        className="group rounded px-2 py-1 -mx-2 -my-1 hover:text-slate-600 hover:underline focus:bg-slate-200 focus:outline-none"
       >
         <h1 className="inline pb-0">{detailName}</h1>
-        <div className="inline-flex items-center">
+        <div className="inline-block align-baseline">
           <img src={linkIcon} alt="Link icon" className="block pl-3 w-10" />
         </div>
       </a>
