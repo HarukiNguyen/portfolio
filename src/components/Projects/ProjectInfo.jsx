64dfbc4fa131df8e587627a23ac362b0960856cb
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-12-11 20:50:59 +0700

    feat: ProjectInfo > adjust space

--- src/components/Projects/ProjectInfo.jsx
+++ src/components/Projects/ProjectInfo.jsx
@@ -5,14 +5,14 @@ import ProjectName from "./ProjectName";
 function ProjectInfo({ name, iconUrl, description, url, completed }) {
   return (
     <div className="absolute bottom-0 translate-y-2/4 bg-slate-50 left-2/4 -translate-x-2/4 w-10/12 rounded-3xl shadow-lg overflow-hidden">
-      <div className="p-7 bg-indigo-100">
+      <div className="pt-7 pb-[14px] px-7 bg-indigo-100">
         <ProjectIcon iconUrl={iconUrl} name={name} />
         <ProjectName name={name} />
         <p className="text-center py-6 text-indigo-700 tablet:text-base ">
           {description}
         </p>
       </div>
-      <div className="text-center p-7">
+      <div className="text-center pt-[14px] pb-7 px-7">
         <ProjectLink url={url} completed={completed} />
       </div>
     </div>
