b797ab9e78811afb2e2dd8913526f7e17b5231ab
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-12-11 17:37:59 +0700

    refactor(HTML): ProjectDetailName > change invalid attr

--- src/components/ProjectIntro/ProjectDetailName.jsx
+++ src/components/ProjectIntro/ProjectDetailName.jsx
@@ -21,23 +21,23 @@ function ProjectDetailName({ url, detailName }) {
             <path
               d="M13.5 10.5L21 3"
               stroke="black"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             />
             <path
               d="M16 3L21 3L21 8"
               stroke="black"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             />
             <path
               d="M21 14V19C21 20.1046 20.1046 21 19 21H12H5C3.89543 21 3 20.1046 3 19V5C3 3.89543 3.89543 3 5 3H10"
               stroke="black"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             />
           </svg>
         </div>
