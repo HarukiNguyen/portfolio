a4a184f7d6666d042c74c5f235a1c086f93ca881
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-12-11 20:44:57 +0700

    feat: ProjectInfo > decorate bg

--- src/components/Projects/ProjectInfo.jsx
+++ src/components/Projects/ProjectInfo.jsx
@@ -4,11 +4,17 @@ import ProjectName from "./ProjectName";
 
 function ProjectInfo({ name, iconUrl, description, url, completed }) {
   return (
-    <div className="absolute bottom-0 translate-y-2/4 bg-slate-50 left-2/4 -translate-x-2/4 w-10/12 p-7 rounded-3xl shadow-lg">
-      <ProjectIcon iconUrl={iconUrl} name={name} />
-      <ProjectName name={name} />
-      <p className="text-center py-6 text-slate-700 tablet:text-base ">{description}</p>
-      <ProjectLink url={url} completed={completed} />
+    <div className="absolute bottom-0 translate-y-2/4 bg-slate-50 left-2/4 -translate-x-2/4 w-10/12 rounded-3xl shadow-lg overflow-hidden">
+      <div className="p-7 bg-indigo-100">
+        <ProjectIcon iconUrl={iconUrl} name={name} />
+        <ProjectName name={name} />
+        <p className="text-center py-6 text-indigo-700 tablet:text-base ">
+          {description}
+        </p>
+      </div>
+      <div className="text-center p-7">
+        <ProjectLink url={url} completed={completed} />
+      </div>
     </div>
   );
 }
--- src/components/Projects/ProjectLink.jsx
+++ src/components/Projects/ProjectLink.jsx
@@ -5,18 +5,16 @@ function ProjectLink({ url, completed }) {
   const linkText = completed ? "view project" : "comming soon";
 
   return (
-    <div className="text-center">
-      <Link
-        to={`${url}`}
-        tabIndex={completed ? 0 : -1}
-        className={`${
-          !completed ? "disable-link text-slate-400 no-underline" : ""
-        } group px-2 py-1 rounded relative underline uppercase tracking-wider font-bold hover:text-indigo-400 focus:hocus-state`}
-      >
-        {linkText}
-        <LinkIcon className="scale-0 absolute w-7 left-full pl-1 top-2/4 -translate-y-2/4 group-hover:scale-100 transition-transform origin-[left_center]" />
-      </Link>
-    </div>
+    <Link
+      to={`${url}`}
+      tabIndex={completed ? 0 : -1}
+      className={`${
+        !completed ? "disable-link text-slate-400 no-underline" : ""
+      } group px-2 py-1 rounded relative underline uppercase tracking-wider font-bold hover:text-indigo-400 focus:hocus-state`}
+    >
+      {linkText}
+      <LinkIcon className="scale-0 absolute w-7 left-full pl-1 top-2/4 -translate-y-2/4 group-hover:scale-100 transition-transform origin-[left_center]" />
+    </Link>
   );
 }
 
