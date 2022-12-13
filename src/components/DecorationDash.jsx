19ebb566d45b186e4d64da69da9e09ca409f6cd1
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-12-13 09:30:03 +0700

    feat: Header > new design at viewport < 340px

---
+++ src/components/DecorationDash.jsx
@@ -0,0 +1,9 @@
+function DecorationDash() {
+  return (
+    <div className="flex w-full items-center">
+      <span className="h-[2px] w-full rounded bg-indigo-600"></span>
+    </div>
+  );
+}
+
+export default DecorationDash;
--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,21 +1,24 @@
+import DecorationDash from '../components/DecorationDash';
 import Logo from '../components/Logo';
 import SocialMedias from '../components/SocialMedias';
 
 function Header() {
   return (
     <header className="layout-container flex animate-fadeIn flex-wrap justify-between py-3 opacity-0 tablet:justify-start tablet:py-5">
-      <div className="flex items-center max-[339px]:pb-3 tablet:pr-10">
+      <div className="flex max-[339px]:w-full max-[339px]:items-center tablet:pr-10">
         <Logo />
+        <div className="hidden grow pl-3 max-[339px]:block">
+          <DecorationDash />
+        </div>
       </div>
-      <div className="relative tablet:pl-7">
-        <span className="absolute left-[6px] top-2/4 h-[6px] w-[6px] -translate-y-2/4 rounded-full bg-indigo-600 min-[340px]:hidden"></span>
+      <div className="relative max-[339px]:ml-auto max-[339px]:flex tablet:pl-7">
         <span className="absolute left-0 top-2/4 hidden h-[6px] w-[6px] -translate-y-2/4 rounded-full bg-indigo-600 tablet:block"></span>
-        <div className="max-[339px]:pl-6">
+        <div className="max-[339px]:-mr-5">
           <SocialMedias />
         </div>
       </div>
-      <div className="hidden tablet:flex tablet:grow tablet:items-center tablet:pl-5">
-        <span className="h-[2px] w-full rounded bg-indigo-600"></span>
+      <div className="hidden pl-5 tablet:flex tablet:grow">
+        <DecorationDash />
       </div>
     </header>
   );
