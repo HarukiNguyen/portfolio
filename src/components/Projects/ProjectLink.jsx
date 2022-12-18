3b1f08302499eb2da473d5646488ee9ad608b363
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-12-18 15:40:49 +0700

    refactor: replace <Link> with <a>
    temporary fix the OtherProjectsList bug
    
    every time re-render, the list will add existing project to the other projects list

--- src/components/Projects/ProjectLink.jsx
+++ src/components/Projects/ProjectLink.jsx
@@ -1,12 +1,11 @@
-import { Link } from 'react-router-dom';
 import LinkIcon from '../LinkIcon';
 
 function ProjectLink({ url, completed }) {
   const linkText = completed ? 'view project' : 'comming soon';
 
   return (
-    <Link
-      to={`${url}`}
+    <a
+      href={`${url}`}
       tabIndex={completed ? 0 : -1}
       className={`${
         !completed ? 'disable-link text-slate-400 no-underline' : ''
@@ -14,7 +13,7 @@ function ProjectLink({ url, completed }) {
     >
       {linkText}
       <LinkIcon className="absolute left-full top-2/4 w-7 origin-[left_center] -translate-y-2/4 scale-0 pl-1 transition-transform group-hover:scale-100" />
-    </Link>
+    </a>
   );
 }
 
